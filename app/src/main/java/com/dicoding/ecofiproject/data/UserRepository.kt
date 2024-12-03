519fe6862e0c37e96c1dd64f544d8b02a35808bd
Masbe993 <nurbayudila93@gmail.com>
2024-12-03 12:54:46 +0700

    fitur login register

--- app/src/main/java/com/dicoding/ecofiproject/data/UserRepository.kt
+++ app/src/main/java/com/dicoding/ecofiproject/data/UserRepository.kt
@@ -6,16 +6,16 @@ import com.dicoding.ecofiproject.data.response.LoginResponse
 import com.dicoding.ecofiproject.data.response.RegisterResponse
 import com.dicoding.ecofiproject.data.pref.UserModel
 import com.dicoding.ecofiproject.data.pref.UserPreference
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import retrofit2.Response
 
 class UserRepository private constructor(
     private val userPreference: UserPreference
 ) {
+
+    // Fungsi login, mengembalikan Response<LoginResponse>
     suspend fun login(email: String, password: String): Response<LoginResponse> {
         return try {
-            val response = ApiConfig.getApiService("").login(email, password)
+            val response = ApiConfig.getApiService().login(email, password)
             Log.d("UserRepository", "Login response: $response")
             response
         } catch (e: Exception) {
@@ -23,9 +23,10 @@ class UserRepository private constructor(
         }
     }
 
+    // Fungsi register, mengembalikan Response<RegisterResponse>
     suspend fun register(name: String, email: String, password: String): Response<RegisterResponse> {
         return try {
-            val response = ApiConfig.getApiService("").register(name, email, password)
+            val response = ApiConfig.getApiService().register(name, email, password)
             Log.d("UserRepository", "Register response: $response")
             response
         } catch (e: Exception) {
@@ -33,27 +34,25 @@ class UserRepository private constructor(
         }
     }
 
+    // Fungsi untuk menyimpan sesi pengguna ke preference
     suspend fun saveSession(user: UserModel) {
         userPreference.saveSession(user)
     }
 
-    fun getSession(): Flow<UserModel> {
-        return userPreference.getSession().map { user ->
-            Log.d("UserRepository", "getSession: User data from preferences: $user")
-            user
-        }
-    }
+    // Fungsi untuk mengambil sesi pengguna dari preference
+    fun getSession() = userPreference.getSession()
 
+    // Fungsi untuk logout, menghapus sesi dari preference
     suspend fun logout() {
         userPreference.logout()
     }
 
+    // Singleton pattern untuk mendapatkan instance UserRepository
     companion object {
         @Volatile
         private var instance: UserRepository? = null
-        fun getInstance(
-            userPreference: UserPreference
-        ): UserRepository =
+
+        fun getInstance(userPreference: UserPreference): UserRepository =
             instance ?: synchronized(this) {
                 instance ?: UserRepository(userPreference)
             }.also { instance = it }
