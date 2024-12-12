020f33aafb0bf8df12c531cf31d3c8c59d166cf9
yavt5610 <133550273+yavt5610@users.noreply.github.com>
2024-12-12 07:14:23 +0700

    SRC delete

--- app/src/main/java/com/dicoding/ecofiproject/data/response/BannersResponse.kt
+++ app/src/main/java/com/dicoding/ecofiproject/data/response/BannersResponse.kt
@@ -1,12 +1,22 @@
 package com.dicoding.ecofiproject.data.response
 
+import com.google.gson.annotations.SerializedName
+
 data class BannersResponse(
+    @SerializedName("status")
     val status: String,
+
+    @SerializedName("message")
     val message: String,
+
+    @SerializedName("data")
     val data: List<Banner>
 ) {
     data class Banner(
+        @SerializedName("id")
         val id: Int,
+
+        @SerializedName("image")
         val image: String
     )
 }
